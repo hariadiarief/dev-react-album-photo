005e156df58712e518982f5d1711f5d4d8dcfc5d
hariadiarief <adi.arief77@gmail.com>
2021-10-05 17:59:38 +0700

    add: about page

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,3 +1,9 @@
 export default function About() {
-    return <div>This is About Page</div>
+    return (
+        <div className='container about'>
+            <a className='about__link' href='https://www.linkedin.com/in/hariadiarief/'>
+                Masagus Hariadi Arief
+            </a>
+        </div>
+    )
 }
